c98ac7913a927cf27a3ddb1b86fc167616b0db0f
jestr <jestr@LAPTOP-ORPA4S2L>
2022-10-27 18:20:09 -0500

    Agregar archivos de proyecto.

---
+++ Access Database/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;//agregamos una libreria.
+
+namespace Access_Database
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Alumno\Downloads\DbPersona.accdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            llenarGrid();
+
+        }
+        void llenarGrid()
+        {
+            conn.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from TablaPersona order by Id", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("Insert into TablaPersona(Nombre,Edad)values('"+textBox2.Text + "'," + textBox3.Text + ")",conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registro exitosamente guardado. ");
+            //limpiar texto();
+            llenarGrid();
+        }
+    }
+}
